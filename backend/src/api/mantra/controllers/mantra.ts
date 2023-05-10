9e0c4e019f7be63b7db89e2698bad4f61978170f
Kaushal Rathi <kaushalrathi24@gmail.com>
2023-05-10 19:38:23 +0530

    feat: add mantra

---
+++ backend/src/api/mantra/controllers/mantra.ts
@@ -0,0 +1,7 @@
+/**
+ * mantra controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::mantra.mantra');
---
+++ backend/src/api/mantra/routes/mantra.ts
@@ -0,0 +1,7 @@
+/**
+ * mantra router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mantra.mantra');
