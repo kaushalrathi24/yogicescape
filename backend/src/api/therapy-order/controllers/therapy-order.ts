f9ad16729cafc61f836a96ddc3f0e737ae75e267
Kaushal Rathi <kaushalrathi24@gmail.com>
2023-05-10 23:41:33 +0530

    feat: add checkout to therapies

--- backend/src/api/therapy-order/controllers/therapy-order.ts
+++ backend/src/api/therapy-order/controllers/therapy-order.ts
@@ -2,6 +2,41 @@
  * therapy-order controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-export default factories.createCoreController('api::therapy-order.therapy-order');
+const getAmmount = async (id) => {
+  return (
+    await strapi.entityService.findOne("api::therapy.therapy", id, {
+      populate: { therapy_type: true },
+    })
+  ).therapy_type.price;
+};
+
+export default factories.createCoreController(
+  "api::therapy-order.therapy-order",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { id } = ctx.request.body;
+      const userId = ctx.state.user.id;
+      const amount = await getAmmount(id);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100,
+        currency: "eur",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      const clientSecret = paymentIntent.client_secret;
+      const data = {
+        user: { connect: [userId] },
+        therapy: { connect: [id] },
+        clientSecret,
+      };
+      await strapi.entityService.create("api::therapy-order.therapy-order", {
+        data,
+      });
+      ctx.send({ clientSecret });
+    },
+  })
+);
