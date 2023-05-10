e3e42f9f33cddb9a5d81c3f5caa0325f6e2b9c0e
Kaushal Rathi <kaushalrathi24@gmail.com>
2023-05-11 00:47:58 +0530

    feat: add checkout to massages

--- backend/src/api/massage-order/controllers/massage-order.ts
+++ backend/src/api/massage-order/controllers/massage-order.ts
@@ -1,7 +1,42 @@
 /**
- * massage-order controller
+ * therapy-order controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-export default factories.createCoreController('api::massage-order.massage-order');
+const getAmmount = async (id) => {
+  return (
+    await strapi.entityService.findOne("api::massage.massage", id, {
+      populate: { massage_type: true },
+    })
+  ).massage_type.price;
+};
+
+export default factories.createCoreController(
+  "api::massage-order.massage-order",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { id } = ctx.request.body;
+      const userId = ctx.state.user.id;
+      const amount = await getAmmount(id);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100,
+        currency: "eur",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      const clientSecret = paymentIntent.client_secret;
+      const data = {
+        user: { connect: [userId] },
+        massage: { connect: [id] },
+        clientSecret,
+      };
+      await strapi.entityService.create("api::massage-order.massage-order", {
+        data,
+      });
+      ctx.send({ clientSecret });
+    },
+  })
+);
